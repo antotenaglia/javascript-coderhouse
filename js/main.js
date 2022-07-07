d524b089979e56a461a4cbb47632be5d8b1d069c
anto tenaglia <antonelatenaglia@gmail.com>
2022-07-06 21:15:51 -0300

    Entrega 1 - Ciclos

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let numero = prompt("Ingrese cantidad de veces que quiere que se repita la palabra Hola")
+
+for (let i = 1; i <= numero; i++) {
+    let salida = "Hola" + " " + i
+    alert(salida);
+}
+
+
+
+
+
+
